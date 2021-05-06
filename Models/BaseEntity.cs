364359afef1a017818d823ecdd16fbbcce76fe1e
ALI KASMOU <alikasmou@gmail.com>
2021-05-07 00:34:23 +0300

    add key to BaseEntity and Contructor for CreateDate and IsDelete

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic;
@@ -8,8 +9,14 @@ namespace ecommerce.Models
 {
     public class BaseEntity<X>
     {
-        public X Id { set; get;  }
+        public BaseEntity()
+        {
+            CreateDate = DateTime.Now;
+            IsDelete = false;
+        }
 
+        [Key]
+        public X Id { set; get;  }
         public DateTime CreateDate { set; get; }
         public DateTime LastModify { get; set; }
         public bool IsDelete { get; set; }  
